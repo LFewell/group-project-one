b4f3950289569b501fc1f523d9e9480a4cd17d4b
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-29 14:08:14 -0400

    worked on fetch

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 var requestUrl = 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=de97dc6df0294b1abbe327d186a0de3d'
+var userInput = document.querySelector(".form-control")
+
+function getRec() {
+    fetch(requestUrl) + "&ingredients=" + userInput
+    .then((response) => response.json())
+    .then ((data) => this.displayRecipe(data));
+};
+
+function displayRecipe() {
+    const { name } = data;
+    console.log(name);
+}
