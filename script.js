f6773c2841caa1e5fb5006c48610ed2678ca2f95
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-30 10:38:35 -0400

    Updated API call function

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 var requestUrl = 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=de97dc6df0294b1abbe327d186a0de3d'
- var userInput; //document.querySelector(".form-control").value
 var button = document.getElementById("test")
-var btn = $("#searched-btn")
+var btn = $("#search-btn")
+
 
 btn.on("click", function(){
-    var btn = $("#searched-btn").val();
-    if (btn !== null) {
-        localStorage.setItem("ingredient", btn)
+    var userInput = document.getElementById('input').value
+    if (userInput !== null) {
+        localStorage.setItem("ingredient", userInput)
     }
     getRec();
-})
-
-
-function getRec() {
-    fetch(requestUrl + "&ingredients=" + "apple")
+    function getRec() {
+    fetch(requestUrl + "&ingredients=" + userInput)
     .then((response) => response.json())
     .then ((data) => console.log(data))
-};
-
-
-// button.addEventListener("click", getRec);
+}});
